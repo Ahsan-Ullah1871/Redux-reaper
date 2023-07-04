d626d08bb57a86ddce9b88a8c780ccd7ee55e9cf
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-07-05 01:55:08 +0600

    router setup done

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
-			<p>Hello world</p>
+			<RouterProvider router={router} />
 		</>
 	);
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+const Home = () => {
+	return <div>Hello new world</div>;
+};
+
+export default Home;
+
